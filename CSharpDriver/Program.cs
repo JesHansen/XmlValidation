618efefe69a6d2f9ff8e24a94404874db267988b
Jes Bak Hansen <jbh@pension.dk>
2022-02-09 23:30:14 +0100

    Initial Commit

---
+++ CSharpDriver/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using FunctionalValidator;
+
+
+var validator = new XML.SchemaValidator(@"C:\src\Skat\rente_schemas\xml\skat2021\view\RenteIndberetningPensiondiverseStrukturType.xsd");
+var result = validator.Check(@"C:\src\Skat\TestXML.xml");
+
+
+var handleOk = (XML.ValidationSucceeded _) =>
+{
+    Console.WriteLine("OK!");
+    return true;
+};
+var handleBadness = (XML.ValidationFailed failure) =>
+{
+    switch (failure)
+    {
+        case XML.ValidationFailed.XmlFileNotFound missing :
+            Console.WriteLine($"{missing.Item}");
+            break;
+        case XML.ValidationFailed.ValidationFailed validationError :
+            Console.WriteLine($"XML file is invalid according to the schema: {validationError.Item}");
+            break;
+    }
+    
+    return false;
+};
+
+result.Match(handleOk, handleBadness);
+
---
+++ FunctionalValidator/Extensions.fs
@@ -0,0 +1,15 @@
+﻿namespace FunctionalValidator
+
+open System
+open System.Runtime.CompilerServices
+    
+type CSharp =        
+    static member ToFSharp (act :Func<'T, 'R>) = act.Invoke
+    
+[<Extension>]  
+module ExtensionMethods =
+    [<Extension>]
+    let Match(result, onOk :Func<'TOk,'R>, onError :Func<'TError, 'R>) =
+        match result with
+        | Ok yay -> (CSharp.ToFSharp onOk) yay
+        | Error boo -> (CSharp.ToFSharp onError) boo
